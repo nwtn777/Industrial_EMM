b21fc5089a24a5cefd6278d4369698e3b9cd3e2c
David Castillo <davocastillo908@gmail.com>
2025-10-27 15:10:27 -0500

    Revert "Add shared processing module and headless video script"
    This reverts commit 88a82c5498922c07dd9a8ec83c5170934681758d.

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,1 @@
-"""Package initialization for src.
-
-Set up a package-level logger so other modules can import `from src import logger`.
-"""
-from .utils import setup_logging
-
-# Initialize a package logger at INFO level by default
-logger = setup_logging()
-
-__all__ = ["logger"]
+# Este archivo permite que src sea tratado como un paquete Python
